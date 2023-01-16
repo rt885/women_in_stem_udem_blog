a30a3c5a4c07aeca9d0cb7e7872a4da1b629b351
rt885 <ruben.t.v@hotmail.com>
2023-01-16 00:25:35 -0600

    imageloader fully deleted

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,16 +5,6 @@ import Link from 'next/link';
 
 const PostCard = ({ post }) => (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
-        {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
-      <Image
-        unoptimized
-        loader={grpahCMSImageLoader}
-        alt={post.title}
-        className="shadow-lg rounded-t-lg lg:rounded-lg"
-        layout="fill"
-        src={post.featuredImage.url}
-      />
-    </div> */}
         <div className="relative overflow-hidden shadow-md pb-80 mb-6">
             <img src={post.featuredImage.url} alt="" className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
         </div>
@@ -26,7 +16,6 @@ const PostCard = ({ post }) => (
             <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
                 <Image
                     unoptimized
-                    loader={grpahCMSImageLoader}
                     alt={post.author.name}
                     height="30px"
                     width="30px"
